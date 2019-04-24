dbb18e7dc3bac0fbc469e11905f8126a551911ce
BhaktiBanugariya <46813380+BhaktiBanugariya@users.noreply.github.com>
2019-04-24 13:46:58 +0530

    Changes related to gauge starting point.

--- projects/ngx-gauge/src/gauge/gauge.ts
+++ projects/ngx-gauge/src/gauge/gauge.ts
@@ -155,7 +155,7 @@ export class NgxGauge implements AfterViewInit, OnChanges, OnDestroy {
             head = Math.PI;
             tail = 2 * Math.PI;
         } else if (type == 'full') {
-            head = 1.5 * Math.PI;
+            head = Math.PI;
             tail = 3.5 * Math.PI;
         } else if (type === 'arch') {
             head = 0.8 * Math.PI;
